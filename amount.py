076b99634aa5b46e012e3495482e781d4258ef8b
PhilipiGomes <philipi.rodrigues342@gmail.com>
2025-12-05 11:55:45 -0300

    Update in the 8x8 hexomino, and the creation of the 4x4, 5x5, 6x6 and 7x7 heptominoes

--- amount.py
+++ amount.py
@@ -1,4 +1,14 @@
+import sys
+
 from polyominoes import ominoes_dict
 
+if len(sys.argv) < 2:
+    sys.exit()
 
-print(len(ominoes_dict[6]))
+n = sys.argv[1]
+
+if n not in ominoes_dict:
+    print("Not avaiable!")
+    sys.exit()
+
+print(len(ominoes_dict[int(n)]))
